851b1cd79df916e82f5b6824d316b7f4c40aaeb2
dkro23 <59180164+dkro23@users.noreply.github.com>
2020-04-01 11:26:25 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -11,6 +11,7 @@
 ### Shiny UI.R
 ### Resource map with DFSS agency data
 ### Code reference: https://github.com/atmajitg/bloodbanks/blob/master/ui.R
+### Geocode reference: https://www.r-bloggers.com/4-tricks-for-working-with-r-leaflet-and-shiny/
 ### Data reference: https://data.cityofchicago.org/Health-Human-Services/Family-and-Support-Services-Delegate-Agencies/jmw7-ijg5/data
 ######
 
@@ -20,5 +21,6 @@ library(leaflet)
 navbarPage("Location of DFSS Agencies", id="main",
            tabPanel("Map", leafletOutput("dfss_map", height=1000)),
            tabPanel("Data", DT::dataTableOutput("data")))
+
 
 
